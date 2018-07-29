86614271b580ff032e88b13bdaf4385cb45ce3c2
Charlie Russo <cparusso@gmail.com>
2018-07-29 05:43:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   end
 
   katz_deli.each_with_index do |person, index|
-    katz_deli << "#{index + 1}. person"
+    person == "#{index + 1}. person"
   end
 
   puts "The line is currently: #{katz_deli}"
