65c41cbe0f8e321cdcdf24f40e3eef8f1fc557ab
Charlie Russo <cparusso@gmail.com>
2018-07-29 05:42:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(katz_deli)
     "The line is currently empty."
   end
 
-  numbered_line = []
-
   katz_deli.each_with_index do |person, index|
     numbered_line << "#{index + 1}. person"
   end
+
+  
 end
